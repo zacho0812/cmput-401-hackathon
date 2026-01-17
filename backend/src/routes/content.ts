5983a3228f7d553c95cd146fce688021dbe7497d
nylinUA <nylin@ualberta.ca>
2026-01-17 13:55:41 -0700

    Added job listing apis, probably wrong idk yall

--- backend/src/routes/content.ts
+++ backend/src/routes/content.ts
@@ -1,30 +1,73 @@
 import { Router } from "express";
 import {PrismaClient} from "@prisma/client"
+import { de } from "zod/locales";
 
 const router = Router();
 
 const prisma = new PrismaClient()
 
-router.get("/api/listings", (req, res) => {
-    
+//I have no clue if the URLS are right or not ngl 
 
-
-});
-
-router.post("/api/listings", (req, res) => {
-
+router.get("/api/listings", async (req, res) => {
+    const job_listings = await prisma.job.findMany({})
+    res.json(job_listings)
 
 });
 
-router.patch("/api/listings/:id", (req, res) => {
+router.post("/api/:userid/listings/add", async (req, res) => {
+    const { position, company, location, deadline, status } = req.body
+    const job_id: string = `${req.params["userid"]}${position}!`;
 
+    const result = await prisma.job.create({
+        data:{
+            id:job_id,
+            userid:req.params["userid"],
+            companyName: company,
+            positiontTitle:position,
+            status: status,
+            location: location,
+            deadline: deadline,
+            dateapplied: new Date(),
+            notes: ""
+
+    },
+
+    })
+    res.json(result)
 
 });
 
-router.delete("/api/listings/:id", (req, res) => {
+router.patch("/api/listings/:id", async (req, res) => {
+    const { position, company, location, deadline, status } = req.body
 
+    const result = await prisma.job.update({
+        where: {id:req.params["id"]},
+        data:{
+            companyName: company,
+            positiontTitle:position,
+            status: status,
+            location: location,
+            deadline: deadline,
+            dateapplied: new Date(),
+            notes: ""
+        },
+    })
+    res.json(result)
 
 });
+
+router.delete("/api/listings/:id", async (req, res) => {
+    const result = await prisma.job.delete({
+        where: {id:req.params["id"]},
+    })
+    res.json(result)
+
+});
+
+
+
+
+
 
 router.get("/api/resume", async (req, res) => {
     const resumes = await prisma.resume.findMany({
@@ -34,22 +77,22 @@ router.get("/api/resume", async (req, res) => {
 
 });
 
-router.get("/api/:userId/resume", async (req, res) => {
+router.get("/api/:userid/resume", async (req, res) => {
     const resumes = await prisma.resume.findMany({
-        where: { userid: req.params["userId"] }
+        where: { userid: req.params["userid"] }
     })
     console.log(resumes)
     res.json(resumes)
 });
 
 
-router.post(`/api/:userId/resume/:resumeId/add`, async (req, res) => {
+router.post(`/api/:userid/resume/:resumeid/add`, async (req, res) => {
     const { resume_data } = req.body
 
     const result = await prisma.resume.create({
     data:{
-        id:req.params["resumeId"],
-        userid:req.params["userId"],
+        id:req.params["resumeid"],
+        userid:req.params["userid"],
         data:resume_data
     }
 
@@ -58,13 +101,13 @@ router.post(`/api/:userId/resume/:resumeId/add`, async (req, res) => {
 })
 
 
-router.patch("/api/:userId/resume/:resumeId/save", async (req, res) => {
+router.patch("/api/:userid/resume/:resumeid/save", async (req, res) => {
     const {resume_data} = req.body
 
     const result = await prisma.resume.update({
-        where: {id:req.params["resumeId"]},
+        where: {id:req.params["resumeid"]},
         data:{
-            userid:req.params["userId"],
+            userid:req.params["userid"],
             data:resume_data
         },
     })
@@ -73,10 +116,10 @@ router.patch("/api/:userId/resume/:resumeId/save", async (req, res) => {
 });
 
 
-router.delete("/api/:userId/resume/:resumeId/delete", async (req, res) => {
+router.delete("/api/:userid/resume/:resumeid/delete", async (req, res) => {
 
     const result = await prisma.resume.delete({
-        where: {id:req.params["resumeId"]},
+        where: {id:req.params["resumeid"]},
     })
     res.json(result)
 
